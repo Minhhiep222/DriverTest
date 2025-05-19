e976f2bfbd73f81e1c4baa122e36f948e21468e6
Minhhiep222 <120901961+Minhhiep222@users.noreply.github.com>
2025-05-19 10:23:23 +0700

    Update driver.ts

--- client/src/apiRequest/driver.ts
+++ client/src/apiRequest/driver.ts
@@ -1,7 +1,6 @@
 
 import { DriverResType, DriverCreateResType, DriverUpdateResType, DriverReadResType, DriveComfirmResType } from "@/schemaValidations/driver.schema";
 import http from "@/lib/http";
-
 const drivers = (params: DriverResType['data'][0]) => {
     // Build query string from params, filtering out undefined values
     const queryParams = Object.entries(params)
@@ -11,8 +10,6 @@ const drivers = (params: DriverResType['data'][0]) => {
 
     return queryParams;
 };
-
-
 const userRequestApi = {
     drivers: (body: { page: number, per_page: number, key: string, filters: DriverResType['data'][0] }) =>
         http.get<DriverResType>(`/tests?search=${body.key}&page=${body.page}&per_page=${body.per_page}&${drivers(body.filters)}`),
@@ -31,12 +28,7 @@ const userRequestApi = {
             `/admin/programs/views?search=${body.key}&page=${body.page}&per_page=${body.per_page}&${drivers(body.filters)}`
         ),
     show: (id: number) => http.get<DriverReadResType>(`/programs/deatail/${id}`),
-
     confirm: (body: DriveComfirmResType) => http.put<DriverReadResType>(`/admin/program/confirm/${body.id}`, body),
-
-    // delete: (body: UserDeleteResType) => http.delete<UserResType>(`users/${body.id}`),
-    // search: (body: { page: number, per_page: number, key: string }) => http.get<UserResType>(`/users?&search=${body.key}&page=${body.page}&per_page=${body.per_page}`),
-
 }
 
 export default userRequestApi;
